15736281449dabf9df3b0f2eb9041c82669a8168
Your Name <you@example.com>
2015-08-25 21:52:35 +0600

    init

---
+++ trunk/java_sub/src/af/sub/SubCase.java
@@ -0,0 +1,24 @@
+/*
+date:2010-3-20
+author:apache.smack
+*/
+
+
+package af.sub;
+
+public class SubCase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static void SysCase()
+	{
+		System.out.println("--e-");
+	}
+}
